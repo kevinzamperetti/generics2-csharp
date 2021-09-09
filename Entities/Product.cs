9b18ad4da13db9a3f38e0753c97c2509fa30f290
Kevin Schepke <kevin.schepke@softplan.com.br>
2021-09-08 23:00:38 -0300

    Example use method generics whit "where T : IComparable"

---
+++ Entities/Product.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Globalization;
+
+namespace Course {
+    public class Product : IComparable {
+      public string Name { get; set; }
+      public double Price { get; set; }
+
+      public Product(string name, double price) {
+          Name = name;
+          Price = price;
+      }
+
+      public override string ToString() {
+        return Name + " , " + Price.ToString("F2", CultureInfo.InvariantCulture);
+      }
+
+      public int CompareTo(object obj) {
+        
+        if (!(obj is Product)) {
+            throw new ArgumentException("Compare error: argument is not a Product!");
+        }
+
+        Product product = obj as Product;
+        return Price.CompareTo(product.Price);
+      }
+  }
+}
